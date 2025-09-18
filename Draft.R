a50dee4ac834f27bf0639e1234e31c447654319c
Waleed Abdulla <waleed.abdulla@slu.edu>
2025-09-18 15:25:46 -0500

    Draft Test

---
+++ Draft.R
@@ -0,0 +1,7 @@
+m <- read.csv("C:/Users/13147/Downloads/Ecuador2017Results(in).csv", strip.white = TRUE, dec = ".",
+              stringsAsFactors = TRUE, encoding="UTF-8",comment.char="")
+
+m <- read.csv("C:/Users/13147/Downloads/Ecuador2017Results(in) (1).csv")
+
+
+
