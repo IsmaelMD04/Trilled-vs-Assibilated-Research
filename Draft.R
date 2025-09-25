9416f255d184195ea56d42febb758f6ef330c2b2
Waleed Abdulla <waleed.abdulla@slu.edu>
2025-09-25 16:46:37 -0500

    Pivot with two variables/values

--- Draft.R
+++ Draft.R
@@ -1,54 +1,30 @@
 library(fpp3)
-library(RCurl)
 m <- read.csv("Ecuador2017Results(in).csv")
- 
-
 View(m)
 
 m2 <- m %>% select(Response.ID:Order, ends_with("fem"))
 View(m2)
 
 m3 <- m2 %>% pivot_longer(
-  cols = ends_with("fem"),
+  cols = ends_with("fem",),
   names_to = "Talker",
   values_to = "Fem"
 )
 
-m %>% pivot_longer(
-  cols = ends_with("fem"),
-  names_to = "Talker",
-  values_to = "Fem"
+#Look at final two columns
+m3 %>% View()
+
+
+#--------------
+
+#Trying to use pivot_longer for two value columns "fem" and "nice"
+t <- m %>% select(Response.ID:Order, ends_with("fem"), ends_with("nice"))
+View(t)
+
+
+#Makes 3,504 entries (wrong) instead of 1,752 entries (right)
+t2 <- t %>% pivot_longer(
+  cols = ends_with(c("fem","nice")),
+  names_to = c("Talker",".value"),
+  names_pattern = "(.*).(....)$",
 ) %>% View()
-
-?pivot_longer()
-
-
-View(m3)
-view(m2)
-m2 <- m %>% pivot_longer(
-  cols = ends_with("fem"), 
-  names_to = c("femininity","niceness","confidence","class","urban","origin","education"),
-  )
-                                                               
-                                                               
-                                                               
-                                                               ,
-                         names_prefix = "fem", values_to = "count")
-
-
-m %>% pivot_longer(cols = Daniela.fem:Andres.edu)
-
-
-m2 %>% select(c(femininity,count))
-
-m2
-pivot_longer(
-  cols = starts_with("wk"),
-  names_to = "week",
-  names_prefix = "wk",
-  values_to = "rank",
-  values_drop_na = TRUE
-)
-
-
-relig_income %>% pivot_longer(!religion, names_to = "income", values_to = "count")
