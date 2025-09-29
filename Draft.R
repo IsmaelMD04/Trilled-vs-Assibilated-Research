2651a6da6a1196594301f98f4cca0927de0896ed
Waleed Abdulla <waleed.abdulla@slu.edu>
2025-09-29 08:30:16 -0500

    Pivoted Values!

--- Draft.R
+++ Draft.R
@@ -18,23 +18,22 @@ m3 %>% View()
 
 #--------------
 
-#Trying to use pivot_longer for every value column accept "eth" (It may cause problems as the only string value column)
-t <- m %>% select(Response.ID:Order, ends_with(c("fem","nice","conf","class","urban","origin","edu","age")))
-#The "." character is associated as a character space in string notation, so something more distinct like "_" is better
+
+t <- m %>% select(Response.ID:Order, ends_with(c("fem","nice","conf","class","urban","origin","edu","age","eth")))
+#The "." character is associated as a character space in string notation, so a more distinct "_" is better
 names(t) <- str_replace_all(names(t), '[.]', "_")
 View(t)
 
 
-#Makes 2,409 entries (wrong) instead of 1,752 entries (right)
-#Likely because "origin" and "age" is found at the end of three columns: Language, Age, Origin
-#The three columns because values in the "Talker" column --> 219 * 11 (8 values + 3 column names)
+#Makes 1,752 entries with pivoted values! Nice
+#"[a-z]_[a-z]" = select columns that contains this pattern: lowerChar_lowerChar 
+#(like Daniel*a*_*f*em but NOT Longtitud*e*_*L*atitude [case sensitive])
+#Talker = The speakers (Daniela, Diego...)
+#.value = take the second part of the column name and use that as the value column(s)
+#names_sep = "_" = divides column names into Talker and .value --> "Daniela_fem" --> Talker: Daniela and value column: fem
 t2 <- t %>% pivot_longer(
-  cols = ends_with(c("fem","nice","conf","class","urban","origin","edu","age")),
+  cols = matches("[a-z]_[a-z]", ignore.case = FALSE),
   names_to = c("Talker",".value"),
-  names_sep = "[_]"
+  names_sep = "_"
 )
 t2 %>% View()
-
-#Task: Find a way to pick values columns in pivot_longer without picking other columns
-  #Change column names?
-  #Function/Specifier?
