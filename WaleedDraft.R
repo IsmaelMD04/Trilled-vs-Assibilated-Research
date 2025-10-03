835b71aedf40080156423b27ad9e25a505e1913a
Waleed Abdulla <waleed.abdulla@slu.edu>
2025-10-03 17:14:00 -0500

    Fixed Response_ID column

---
+++ WaleedDraft.R
@@ -0,0 +1,21 @@
+library(fpp3)
+library(tidyverse)
+m <- read.csv("Ecuador2017Results(in).csv")
+
+t <- m %>% select(-c(Start.Date, End.Date, Finished, IP.Address, Duration..in.seconds., Location.Latitude, 
+                     Location.Longitude, ends_with(".eth"), Order)) %>% filter(Progress >= 74)
+#The "." character is associated as a character space in string notation, so a more distinct "_" is better
+names(t) <- str_replace_all(names(t), '[.]', "_")
+
+
+#Makes 1,752 entries with pivoted values! Nice
+#"[a-z]_[a-z]" = select columns that contains this pattern: lowerChar_lowerChar 
+#(like Daniel*a*_*f*em but NOT Longtitud*e*_*L*atitude [case sensitive])
+#Talker = The speakers (Daniela, Diego...)
+#.value = take the second part of the column name and use that as the value column(s)
+#names_sep = "_" = divides column names into Talker and .value --> "Daniela_fem" --> Talker: Daniela and value column: fem
+t2 <- t %>% pivot_longer(
+  cols = matches("[a-z]_[a-z]", ignore.case = FALSE),
+  names_to = c("Talker",".value"),
+  names_sep = "_") %>% 
+  mutate(Response_ID = match(Response_ID, Response_ID %>% unique()))
