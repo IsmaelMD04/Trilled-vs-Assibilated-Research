34ee4739b399002206fb4fbaf4765444651b69ad
Waleed Abdulla <waleed.abdulla@slu.edu>
2025-10-09 16:51:05 -0500

    origin and column names fixed

--- Draft.R
+++ Draft.R
@@ -3,24 +3,32 @@ library(tidyverse)
 library(stringr)
 m <- read.csv("Ecuador2017Results(in).csv")
 
-t <- m %>% select(-c(Start.Date, End.Date, Finished, IP.Address, Duration..in.seconds., Location.Latitude, 
-                     Location.Longitude, ends_with(".eth"), Order)) %>% filter(Progress >= 74)
+t <- m %>% 
+  select(-c(Start.Date, End.Date, Finished, Speaker, IP.Address, Duration..in.seconds., 
+            Location.Latitude, Location.Longitude, ends_with(".eth"))) %>% 
+  filter(Progress >= 74) %>%
+  rename(c(RespondentID = Response.ID, PredictedRegion = Region, RespondentBackground = Origin))
+
 #The "." character is associated as a character space in string notation, so a more distinct "_" is better
 names(t) <- str_replace_all(names(t), '[.]', "_")
 
 
-#Makes 1,752 entries with pivoted values! Nice
+
 #"[a-z]_[a-z]" = select columns that contains this pattern: lowerChar_lowerChar 
-#(like Daniel*a*_*f*em but NOT Longtitud*e*_*L*atitude [case sensitive])
-#Talker = The speakers (Daniela, Diego...)
+#(like Daniel*a*_*f*em)
+#Speaker = The speakers (Daniela, Diego...)
 #.value = take the second part of the column name and use that as the value column(s)
-#names_sep = "_" = divides column names into Talker and .value --> "Daniela_fem" --> Talker: Daniela and value column: fem
+#names_sep = "_" = divides column names into Talker and .value
+  #Example) "Daniela_fem" --> Talker: Daniela and value column: fem
 t2 <- t %>% pivot_longer(
   cols = matches("[a-z]_[a-z]", ignore.case = FALSE),
-  names_to = c("Talker",".value"),
+  names_to = c("Speaker",".value"),
   names_sep = "_") %>% 
-  mutate(Response_ID = match(Response_ID, Response_ID %>% unique())) %>%
-  relocate(Region, .after = last_col())
+  mutate(RespondentID = match(RespondentID, RespondentID %>% unique())) %>%
+  relocate(PredictedRegion, .after = last_col()) %>%
+  mutate(origin = str_replace_all(origin %>% as.character(), 
+                           c("1" = "Quito", "2" = "Cuenca", 
+                             "3" = "Loja", "4" = "Other")))
 
 responder_Origins <- c("Espaí", "Cariamanga", "Loja", "Guayaquil", 
                        "Honduras", "Machala", "Ambato", "Arenillas",
@@ -41,10 +49,10 @@ responder_Origins <- c("Espaí", "Cariamanga", "Loja", "Guayaquil",
                        "Tegucigalpa", "Alemania", "Holanda", 
                        "Santo Domingo de los Tsíçchila", "Espana", "Costa",
                        "Selva Alegre", "Shell", "Zapotillo", "Zaruma")
-t3 <- t2 %>% mutate(Origin = str_extract_all(Origin, 
+
+t3 <- t2 %>% mutate(RespondentBackground = str_extract_all(RespondentBackground, 
              regex(str_c(responder_Origins, collapse = "|"), 
-             ignore_case = TRUE)) %>% sapply((function(x) paste(unique(x),
-                                                                                                                collapse = ", "))))
+             ignore_case = TRUE)) %>% sapply((function(x) paste(unique(x), collapse = ", "))))
 t3 <- t3 %>%
-  mutate(Origin = na_if(Origin, ""))
+  mutate(RespondentBackground = na_if(RespondentBackground, ""))
 t3 %>% View()
