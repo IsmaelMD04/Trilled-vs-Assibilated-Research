1864e915c4178c577319c58f233e74c98fa3e8d1
Clara <claralederer1@gmail.com>
2025-10-15 19:56:17 -0500

    Basic language grouping

---
+++ ClaraDraft.R
@@ -0,0 +1,90 @@
+library(fpp3)
+library(tidyverse)
+library(stringr)
+m <- read.csv("Ecuador2017Results(in).csv")
+
+t <- m %>% 
+  select(-c(Start.Date, End.Date, Finished, Speaker, IP.Address, Duration..in.seconds., 
+            Location.Latitude, Location.Longitude, ends_with(".eth"))) %>% 
+  filter(Progress >= 74)
+
+#The "." character is associated as a character space in string notation, so a more distinct "_" is better
+names(t) <- str_replace_all(names(t), '[.]', "_")
+
+#"[a-z]_[a-z]" = select columns that contains this pattern: lowerChar_lowerChar (like Daniel*a*_*f*em)
+#Speaker = The speakers (Daniela, Diego...)
+#.value = take the second part of the column name and use that as the value column(s)
+#names_sep = "_" = divides column names into Talker and .value --> "Daniela_fem" --> Talker: Daniela / Value: fem
+
+t2 <- t %>% pivot_longer(
+  cols = matches("[a-z]_[a-z]", ignore.case = FALSE),
+  names_to = c("Speaker",".value"),
+  names_sep = "_") %>%
+  rename(c(RespondentID = Response_ID, RespondentBackground = Origin), predictedOrigin = origin) %>%
+  mutate(RespondentID = match(RespondentID, RespondentID %>% unique())) %>%
+  relocate(predictedOrigin, .after = last_col()) %>%
+  mutate(predictedOrigin = str_replace_all(predictedOrigin %>% as.character(), 
+                                           c("1" = "Quito", "2" = "Cuenca", "3" = "Loja", "4" = "Other")),
+         edu = str_replace_all(edu %>% as.character(),
+                               c("1" = "Primary", "2" = "Secondary", "3" = "University", 
+                                 "4" = "Mastery", "5" = "Doctorate")),
+         age = str_replace_all(age %>% as.character(),
+                               c("1" = "15-19", "2" = "20-29", "3" = "30-39", 
+                                 "4" = "40-49", "5" = "50-59", "6" = "60+")),
+         trill = (str_replace_all(Speaker, c(
+           "Sofia" = "0", "Andrea" = "0", "Andres" = "0", "Diego" = "0",
+           "Isabel" = "1", "Daniela" = "1", "Carlos" = "1", "Pablo" = "1")
+         )) %>% as.integer(),
+         Speaker = str_replace_all(Speaker, c(
+           "Sofia" = "F1", "Andrea" = "F2", "Andres" = "M1", "Diego" = "M2",
+           "Isabel" = "F1", "Daniela" = "F2", "Carlos" = "M1", "Pablo" = "M2"))
+  ) %>% relocate(trill, .before = "Speaker")
+
+
+
+responder_Origins <- c("Espaí", "Cariamanga", "Loja", "Guayaquil", 
+                       "Honduras", "Machala", "Ambato", "Arenillas",
+                       "Huaquillas", "Argentina", "Cariamanga",
+                       "Catacocha", "Catamayo", "Zamora", "Quito", 
+                       "Cuenca", "Duran", "EEUU", "USA", "Puerto Rico",
+                       "Sucia", "San Juan Bosco", "Mexico", "Chile",
+                       "Inglaterra", "Colombia", "El Salvador",
+                       "Nicaragua", "Bolivia", "Pií", "Portovelo", 
+                       "Saraguro", "Esmeraldas", "Guachapala", "Palanda",
+                       "Gualaceo", "Guaranda", "Coimbra", "Ibarra", "Lago",
+                       "Agrio", "Santa Rosa", "El Oro", "Míçchala", 
+                       "Londres", "Macara", "Macas", "San Lucas", "Madrid",
+                       "Manta", "Portoviejo", "Jipijapa", "Canton",
+                       "Tulciç", "Puyo", "Riobamba",
+                       "Puerto el Carmen del Putumayo", "Gualaquiza",
+                       "Galíçpagos", "Latacunga", "Changaimina", "Oriente",
+                       "Tegucigalpa", "Alemania", "Holanda", 
+                       "Santo Domingo de los Tsíçchila", "Espana", "Costa",
+                       "Selva Alegre", "Shell", "Zapotillo", "Zaruma")
+
+t3 <- t2 %>% mutate(
+  RespondentBackground = 
+    str_extract_all(RespondentBackground, regex(str_c(responder_Origins, collapse = "|"), ignore_case = TRUE)) %>%
+    sapply((function(x) paste(unique(x), collapse = ", "))),
+  lang_std = iconv(Language, from = "UTf-8", to = "ASCII//TRANSLIT") %>%
+    tolower() %>%
+    str_squish(),
+  mspanish  = str_detect(lang_std, "\\b(spanish|espanol|castellano)\\b"),
+  menglish  = str_detect(lang_std, "\\b(english|ingles)\\b"),
+  mspanglish = str_detect(lang_std, "\\bspanglish\\b"),
+  lang_g = case_when(
+    is.na(lang_std) | lang_std == "" ~ NA_character_,
+    mspanglish ~ "Both Spanish and English",
+    mspanish & menglish ~ "Both Spanish and English",
+    mspanish ~ "Spanish",
+    menglish ~ "English",
+    TRUE ~ "Other Language"
+  )
+)
+
+t3 <- t3 %>%
+  mutate(RespondentBackground = na_if(RespondentBackground, ""))
+t3 %>% View()
+
+t3 %>% str()
+
