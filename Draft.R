353e4b6501b88a4ef7f09726e7f70933cc284848
Waleed Abdulla <waleed.abdulla@slu.edu>
2025-10-20 13:53:37 -0500

    Fixed code up to Lang Org

--- Draft.R
+++ Draft.R
@@ -6,29 +6,36 @@ m <- read.csv("Ecuador2017Results(in).csv")
 t <- m %>% 
   select(-c(Start.Date, End.Date, Finished, Speaker, IP.Address, Duration..in.seconds., 
             Location.Latitude, Location.Longitude, ends_with(".eth"))) %>% 
-  filter(Progress >= 74) %>%
-  rename(c(RespondentID = Response.ID, PredictedRegion = Region, RespondentBackground = Origin))
+  filter(Progress >= 74)
 
 #The "." character is associated as a character space in string notation, so a more distinct "_" is better
 names(t) <- str_replace_all(names(t), '[.]', "_")
 
-
-
-#"[a-z]_[a-z]" = select columns that contains this pattern: lowerChar_lowerChar 
-#(like Daniel*a*_*f*em)
+#"[a-z]_[a-z]" = select columns that contains this pattern: lowerChar_lowerChar (like Daniel*a*_*f*em)
 #Speaker = The speakers (Daniela, Diego...)
 #.value = take the second part of the column name and use that as the value column(s)
-#names_sep = "_" = divides column names into Talker and .value
-  #Example) "Daniela_fem" --> Talker: Daniela and value column: fem
+#names_sep = "_" = divides column names into Talker and .value --> "Daniela_fem" --> Talker: Daniela / Value: fem
+
 t2 <- t %>% pivot_longer(
   cols = matches("[a-z]_[a-z]", ignore.case = FALSE),
   names_to = c("Speaker",".value"),
-  names_sep = "_") %>% 
+  names_sep = "_") %>%
+  rename(c(RespondentID = Response_ID, RespondentBackground = Origin), predictedOrigin = origin) %>%
   mutate(RespondentID = match(RespondentID, RespondentID %>% unique())) %>%
-  relocate(PredictedRegion, .after = last_col()) %>%
-  mutate(origin = str_replace_all(origin %>% as.character(), 
-                           c("1" = "Quito", "2" = "Cuenca", 
-                             "3" = "Loja", "4" = "Other")))
+  relocate(predictedOrigin, .after = last_col()) %>%
+  mutate(predictedOrigin = str_replace_all(predictedOrigin %>% as.character(), 
+                                           c("1" = "Quito", "2" = "Cuenca", "3" = "Loja", "4" = "Other")),
+         trill = (str_replace_all(Speaker, c(
+           "Sofia" = "0", "Andrea" = "0", "Andres" = "0", "Diego" = "0",
+           "Isabel" = "1", "Daniela" = "1", "Carlos" = "1", "Pablo" = "1")
+         )) %>% as.integer(),
+         Speaker = str_replace_all(Speaker, c(
+           "Sofia" = "F1", "Andrea" = "F2", "Andres" = "M1", "Diego" = "M2",
+           "Isabel" = "F1", "Daniela" = "F2", "Carlos" = "M1", "Pablo" = "M2"))
+  ) %>% 
+  relocate(trill, .before = Speaker)
+
+
 
 responder_Origins <- c("Espaí", "Cariamanga", "Loja", "Guayaquil", 
                        "Honduras", "Machala", "Ambato", "Arenillas",
@@ -50,44 +57,18 @@ responder_Origins <- c("Espaí", "Cariamanga", "Loja", "Guayaquil",
                        "Santo Domingo de los Tsíçchila", "Espana", "Costa",
                        "Selva Alegre", "Shell", "Zapotillo", "Zaruma")
 
-t3 <- t2 %>% mutate(RespondentBackground = str_extract_all(RespondentBackground, 
-             regex(str_c(responder_Origins, collapse = "|"), 
-             ignore_case = TRUE)) %>% sapply((function(x) paste(unique(x), collapse = ", "))))
-t3 <- t3 %>% mutate(RespondentBackground = str_replace_all(RespondentBackground 
-             %>% as.character(), c("Espaí" = "Spain", "Espana" = "Spain", 
-             "EEUU" = "USA", "Míçchala" = "Machala", "Galíçpagos" = "Galapagos",
-             "Alemania" = "Germany", "Holanda" = "Netherlands", "Madrid" = "Spain",
-             "Tsíçchila" = "Tsachilas")))
-
 t3 <- t2 %>% mutate(
   RespondentBackground = 
     str_extract_all(RespondentBackground, regex(str_c(responder_Origins, collapse = "|"), ignore_case = TRUE)) %>%
-    sapply((function(x) paste(unique(x), collapse = ", "))),
-  lang_std = iconv(Language, from = "UTf-8", to = "ASCII//TRANSLIT") %>%
-    tolower() %>%
-    str_squish(),
-  mspanish  = str_detect(lang_std, "\\b(spanish|espanol|castellano)\\b"),
-  menglish  = str_detect(lang_std, "\\b(english|ingles)\\b"),
-  mspanglish = str_detect(lang_std, "\\bspanglish\\b"),
-  lang_g = case_when(
-    is.na(lang_std) | lang_std == "" ~ NA_character_,
-    mspanglish ~ "Both Spanish and English",
-    mspanish & menglish ~ "Both Spanish and English",
-    mspanish ~ "Spanish",
-    menglish ~ "English",
-    TRUE ~ "Other Language"
-  )
+    sapply((function(x) paste(unique(x), collapse = ", ")))
 )
 
 t3 <- t3 %>%
-  mutate(RespondentBackground = na_if(RespondentBackground, ""))
+  mutate(RespondentBackground = na_if(RespondentBackground, ""),
+         RespondentBackground = coalesce(RespondentBackground, Region))
+
+
 t3 %>% View()
-
-
-t3 <- t3 %>% mutate(RespondentBackground = coalesce(RespondentBackground, Region))
-t3 <- t3 %>% select(-c(Region))
-t3 %>% View()
-
 
 #writing csv for unique values of language since there's so much of them since questions are in free response form (running this code resets the csv file so I commented it out)
 #write.csv(data.frame(language_old = unique(t3$Language)), "Educador_language_cols.csv", row.names = FALSE)
