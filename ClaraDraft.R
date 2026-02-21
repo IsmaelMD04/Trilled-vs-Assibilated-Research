68fd4e8638513143127c92fb9564500ed64e6399
Clara <claralederer1@gmail.com>
2026-02-21 11:14:38 -0600

    Factor Analysis

--- ClaraDraft.R
+++ ClaraDraft.R
@@ -145,3 +145,46 @@ view(age_by_region)
 
 age_by_region %>% ggplot(aes(x = Region, y = proportion, fill = age_group)) + 
   geom_col(position = "fill") + labs(title = "Age distribution by region", x = "Region", y = "Proportion")
+
+# Begin factor analysis 
+# Apply regression on all dependent variables (fem - age, origin is kept categorical), 
+# group/average based on sway towards the other variables, and assess the effect of trill or not 
+
+# Invert femininity rating 
+# Split speaker into speaker and speaker gender variable 
+
+t4 <- t3 |> mutate(masculinity = 7 - fem)
+t4 <- t4 |> select(-fem)
+t4 <- t4 |> mutate(speakerID = Speaker, 
+                   speaker_gender = ifelse(str_starts(Speaker, 'F'), 'female', 'male'))
+t4 <- t4 |> select(-Speaker)
+
+# picking only numeric variables
+fadata <- t4 |> select(masculinity, nice, class, urban, edu, age)
+str(fadata)
+colSums(is.na(fadata))
+summary(fadata)
+
+# dropping na
+fadata <- fadata |> drop_na()
+
+fa <- factanal(fadata, factors = 2, rotation = 'varimax')
+fa
+# class, urban, and edu are a status factor 
+# higher masculinity = lower nice
+# more feminine = higher nice
+# age isn't strong 
+
+# Factor 1: Status
+# Factor 2: "Gendered Affect" or something 
+
+t4 <- t4 |> mutate(status = rowMeans(scale(select(., class, urban, edu)), na.rm = TRUE))
+
+# mixed effect/ random effect 
+# linear model (status ~ trill + listener_id)
+# 2 level logistic regression
+# glm(response ~ predictors, family = 'binomial')
+# create new variable that says if respondent correctly assigned speaker's origin 
+
+
+
